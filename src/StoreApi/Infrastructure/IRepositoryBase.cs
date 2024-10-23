31030c24e0e54034aa8a2b61285c1b626c643510
Hasan Çoban <hasancoban13579@gmail.com>
2024-10-23 23:50:05 +0300

    Implement base repository interface methods

--- src/StoreApi/Infrastructure/IRepositoryBase.cs
+++ src/StoreApi/Infrastructure/IRepositoryBase.cs
@@ -1,8 +1,23 @@
-﻿namespace StoreApi.Infrastructure
+﻿using System.Linq.Expressions;
+
+namespace StoreApi.Infrastructure
 {
     public interface IRepositoryBase<T>
     {
-        Task<IQueryable<T>> FindAllAsync();
-        Task<IQueryable> FindByConditionAsync();   
+        public IQueryable<T> FindAll();
+        /// <summary>
+        /// Makes a query to the database using the 
+        /// provided expression
+        /// 
+        /// e.g: _customerRepository.FindByCondition(c => c.Id = givenId)
+        /// this is queried as DbSet.Where(c => c.Id = givenId)
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        
+        public void Create(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
     }
 }
--- src/StoreApi/Infrastructure/RepositoryBase.cs
+++ src/StoreApi/Infrastructure/RepositoryBase.cs
@@ -1,6 +1,40 @@
-﻿namespace StoreApi.Infrastructure
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace StoreApi.Infrastructure
 {
-    public class RepositoryBase
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
+        protected readonly StoreContext _storeContext;
+
+        public RepositoryBase(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+        public void Create(T entity)
+        {
+            _storeContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _storeContext.Remove(entity);
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return _storeContext.Set<T>();
+        }
+        public void Update(T entity)
+        {
+            _storeContext.Set<T>().Update(entity);
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+
+            return _storeContext.Set<T>().Where(expression);
+        }
+
     }
 }
