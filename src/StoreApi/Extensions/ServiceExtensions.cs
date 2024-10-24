a3b990c1cab0904675c750c4596291a15d3cbae6
Hasan Çoban <hasancoban13579@gmail.com>
2024-10-25 02:55:26 +0300

    Add configuration methods for adding services and repositories via dependency injection.

--- src/StoreApi/Extensions/ServiceExtensions.cs
+++ src/StoreApi/Extensions/ServiceExtensions.cs
@@ -1,4 +1,12 @@
-﻿namespace StoreApi.Extensions
+﻿using StoreApi.Features;
+using StoreApi.Features.Carts;
+using StoreApi.Features.Categories;
+using StoreApi.Features.Customers;
+using StoreApi.Features.Orders;
+using StoreApi.Features.Products;
+using StoreApi.Features.Wishlists;
+
+namespace StoreApi.Extensions
 {
     public static class ServiceExtensions
     {
@@ -11,6 +19,28 @@
                     builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
                 );
             });
+        }
+
+        public static void AddEntityRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IWishlistRepository, WishlistRepository>();
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
+
+        public static void AddEntityServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICartService, CartService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IWishlistService, WishlistService>();
+            services.AddScoped<IServiceManager, ServiceManager>();
         }
     }
 }
