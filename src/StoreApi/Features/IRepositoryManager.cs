c810decb739ab56bbf2d12a76ca66028e0440809
Hasan Çoban <hasancoban13579@gmail.com>
2024-10-25 02:34:45 +0300

    Create interfaces for service and repository managers.

---
+++ src/StoreApi/Features/IRepositoryManager.cs
@@ -0,0 +1,19 @@
+﻿using StoreApi.Features.Carts;
+using StoreApi.Features.Categories;
+using StoreApi.Features.Customers;
+using StoreApi.Features.Orders;
+using StoreApi.Features.Products;
+using StoreApi.Features.Wishlists;
+
+namespace StoreApi.Features
+{
+    public interface IRepositoryManager
+    {
+        IProductRepository ProductRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+        IOrderRepository OrderRepository { get; }
+        IWishlistRepository WishlistRepository { get; }
+        ICartRepository CartRepository { get; }
+        ICustomerRepository CustomerRepository { get; }
+    }
+}
---
+++ src/StoreApi/Features/IServiceManager.cs
@@ -0,0 +1,19 @@
+﻿using StoreApi.Features.Carts;
+using StoreApi.Features.Categories;
+using StoreApi.Features.Customers;
+using StoreApi.Features.Orders;
+using StoreApi.Features.Products;
+using StoreApi.Features.Wishlists;
+
+namespace StoreApi.Features
+{
+    public interface IServiceManager
+    {
+        IProductService ProductService { get; }
+        IOrderService OrderService { get; }
+        ICustomerService CustomerService { get; }
+        ICartService CartService { get; }
+        IWishlistService WishlistService { get; }
+        ICategoryService CategoryService { get; }
+    }
+}
